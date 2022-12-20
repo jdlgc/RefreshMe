3c12a22508bf3e8cbb4d55dbc4f92099451b3772
leticia <leticiacohenferrari@gmail.com>
2022-12-20 15:22:22 +0100

    feature(webpages): read and write pages into database

--- src/main/java/com/upm/refreshme/backend/Servlet.java
+++ src/main/java/com/upm/refreshme/backend/Servlet.java
@@ -1,9 +1,15 @@
 package com.upm.refreshme.backend;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 import javax.mail.Message;
@@ -13,20 +19,21 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 
+import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.EmailIdentifier;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.GetUsersResult;
 import com.google.firebase.auth.UserRecord;
 import com.google.firebase.auth.UserRecord.CreateRequest;
+import com.google.firebase.cloud.FirestoreClient;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Servlet {
@@ -34,46 +41,39 @@ public class Servlet {
 	private static UserRecord userRecord;
 	private static FirebaseAuth defaultAuth;
 	private static FirebaseDatabase defaultDatabase;
+	private static Firestore db;
 
 	final static String rmEmail = "refreshmeapp@gmail.com";
 	final static String token = "jprafjwywgsijwci";
-
-	public static void main(String[] args) throws IOException{
-
-		FileInputStream serviceAccount = new FileInputStream("./ServiceAccountKey.json");
-
-		FirebaseOptions options = FirebaseOptions.builder()
-				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				.setDatabaseUrl("https://refreshme-8db78-default-rtdb.europe-west1.firebasedatabase.app/")
-				.build();
-
-		// Initialize the default app
-		FirebaseApp defaultApp = FirebaseApp.initializeApp(options);
-
-
-		System.out.println(defaultApp.getName());  // "[DEFAULT]"
-
-		// Retrieve services by passing the defaultApp variable...
-		defaultAuth = FirebaseAuth.getInstance(defaultApp);
-		defaultDatabase = FirebaseDatabase.getInstance(defaultApp);
-		
-		String password = "123456";
-		
-		if (password.length() < 6) {
-			System.out.println("Por favor, introduzca  una contraseña de al menos 6 caracteres.");
-		} else {
-			try {
-				createUser("John Doe", "jorcordd@gmail.com", password);
-			} catch (FirebaseAuthException e) {
-				switch(e.getMessage()) {
-				case "The user with the provided email already exists (EMAIL_EXISTS).":
-					System.out.println("Correo ya registrado, por favor, introduzca un correo no existente en RefreshMe.");
-					break;
-				default:
-					System.out.println(e.getMessage());
-				}
-			}
+	
+	public Servlet() {
+		db = initiateFirebase();
+	}
+	
+	private static Firestore initiateFirebase() {
+		FileInputStream serviceAccount;
+		try {
+			serviceAccount = new FileInputStream("./ServiceAccountKey.json");
+			FirebaseOptions options = FirebaseOptions.builder()
+					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+					.setDatabaseUrl("https://refreshme-8db78-default-rtdb.europe-west1.firebasedatabase.app/")
+					.build();
+			
+			// Initialize the default app
+			FirebaseApp defaultApp = FirebaseApp.initializeApp(options);
+			
+			
+			System.out.println(defaultApp.getName());  // "[DEFAULT]"
+			
+			// Retrieve services by passing the defaultApp variable...
+			defaultAuth = FirebaseAuth.getInstance(defaultApp);
+			defaultDatabase = FirebaseDatabase.getInstance(defaultApp);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+
+		return FirestoreClient.getFirestore();
 	}
 
 	private static void createUser(String userName, String email, String password) throws FirebaseAuthException {
@@ -150,6 +150,51 @@ public class Servlet {
 		} catch (MessagingException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static String getUser() {
+		// TODO
+		return "1";
+	}
+	
+	public boolean addNewWebPage(WebPage webPage) {
+		DocumentReference docRef = db.collection("users").document(getUser())
+				.collection("web-pages").document(UUID.randomUUID().toString());
+
+		Map<String, Object> data = new HashMap<>();
+        data.put("nombre", webPage.getNombre());
+        data.put("categoria", webPage.getCategoria());
+        data.put("url", webPage.getUrl());
+        data.put("ultimosCambios", webPage.getUltimosCambios());
+        
+        ApiFuture<WriteResult> result = docRef.set(data);
+
+        try {
+			System.out.println("Update time : " + result.get().getUpdateTime());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+        return true;
+	}
+	
+	public ArrayList<WebPage> getWebPagesList() {
+		ArrayList<WebPage> webPages = new ArrayList<WebPage>();
+		try {
+			ApiFuture<QuerySnapshot> future = db.collection("users").document(getUser())
+					.collection("web-pages").get();
+			List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+			for (QueryDocumentSnapshot document : documents) {
+				WebPage webPage = new WebPage(document.getData().get("nombre").toString(), 
+						document.getData().get("url").toString(), 
+						document.getData().get("categoria").toString(), 
+						document.getData().get("ultimosCambios").toString());
+				webPages.add(webPage);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return webPages;
 	}
 
 
